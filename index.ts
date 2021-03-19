54f51c3562ddee31223ed47db42e7c0e0663e049
Humaira Summit <h.specials@gmail.com>
2021-03-19 15:07:28 -0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from 'apollo-server';
+import { ApolloGateway } from '@apollo/gateway';
+
+const isProd = process.env.NODE_ENV === "production";
+if(!isProd)require('dotenv').config();
+
+const gateway = new ApolloGateway({debug: !isProd});
+const server = new ApolloServer({
+    gateway,
+    subscriptions: false
+});
+    
+const port = process.env.PORT || 4000;
+server.listen({ port }).then(({ url }) => {
+    console.log(`🚀 Gateway ready at ${url}`);
+});
---
+++ services/accounts/src/index.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+import { gql, ApolloServer } from 'apollo-server';
+import { buildFederatedSchema } from '@apollo/federation';
+
+const typeDefs = gql(readFileSync(resolve(__dirname, "./schema.graphql"), { encoding: "utf8" }));
+const server = new ApolloServer({
+    schema: buildFederatedSchema([{ typeDefs }]),
+    mocks: true,
+    mockEntireSchema: false,
+    engine: false,
+    });
+    
+const port = process.env.PORT || 4004;
+server.listen({ port }).then(({ url }) => {
+    console.log(`🚀 accounts service ready at ${url}`);
+});
